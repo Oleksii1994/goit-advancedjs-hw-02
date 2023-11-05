91d057e7df336f0563eff74bde4036b4a6ed6209
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-11-05 15:39:26 +0200

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+  let delayForNextPromise = Number(delay.value);
+
+  for (let i = 1; i <= Number(amount.value); i += 1) {
+    createPromise(i, delayForNextPromise)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          position: 'topRight',
+          message: `✅ Fulfilled promise ${position} in ${delay}ms`,
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          position: 'topRight',
+          message: `❌ Rejected promise ${position} in ${delay}ms`,
+        });
+      });
+    delayForNextPromise += Number(step.value);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
